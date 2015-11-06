76825f88e8dbe16ede7797caac2a940f725b703e
Graphic <graphic@SOD-SJH-516-15M.local>
2015-11-06 14:59:11 -0500

    final for class

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //What are you trying to do?, the images are already width divided?  You need to reduce or adjust the lenght so they are squares
-$(document).ready(function(){
+$(document).ready(function (){
 	var match = $('.galimg').css('width');
 	var resizeboxes = function() {
 	
@@ -7,11 +7,15 @@ $(document).ready(function(){
 
 };
 	$('.galimg').each(resizeboxes);
+	var width = $('.galimg img').css('width');
+	var height = $('.galimg img').css('height'); //this does not check per image
+	
+	$('figure:firstChild').addClass('portrait');
 });
 
 
 
-
+//so far, image containers are all square and resize correctly, but the images are now too big
 // if width > height (apply .landscape)
 // if height > width (apply .portrait)
 // for all cases, after if, center inside .galimg
